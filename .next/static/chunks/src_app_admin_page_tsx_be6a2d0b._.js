1680f9bc8cfd2249b8d10023104e9109032f39d1
yashikassdn-ux <yashikassdn@gmail.com>
2025-08-11 13:33:33 +0530

    Add .next/static/chunks/src_app_admin_page_tsx_be6a2d0b._.js 1754899412994

---
+++ .next/static/chunks/src_app_admin_page_tsx_be6a2d0b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c54b8c06._.js",
+  "static/chunks/src_7e1c1f87._.js"
+],
+    source: "dynamic"
+});
